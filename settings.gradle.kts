cd3eeac7934670d53e15a85c3763edcfea8717b0
Gaston Thea <gaston.thea@harness.io>
2026-03-04 21:09:53 -0300

    Retryable HTTP deps

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -25,5 +25,13 @@ rootProject.name = "android-thin-client"
 include(":android-thin-client")
 include(":api")
 // include(":sdk")
-include(":android-client:fallback")
-include(":android-client:logger")
+
+listOf<String>(
+    "fallback",
+    "logger",
+    "http",
+    "http-api",
+    "backoff"
+).forEach {
+    include(":android-client:$it")
+}
