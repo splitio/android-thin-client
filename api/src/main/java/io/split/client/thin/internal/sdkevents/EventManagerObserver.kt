ac0a9bf5bfd9f579f306666917f518a887202bfa
Gaston Thea <gaston.thea@harness.io>
2026-03-19 10:22:21 -0300

    Extract Observable event types to observable module

--- api/src/main/java/io/split/client/thin/internal/sdkevents/EventManagerObserver.kt
+++ api/src/main/java/io/split/client/thin/internal/sdkevents/EventManagerObserver.kt
@@ -3,6 +3,7 @@ package io.split.client.thin.internal.sdkevents
 import io.harness.events.EventsManager
 import io.split.client.thin.SplitEvent
 import io.split.client.thin.internal.observer.ObservableEvent
+import io.split.client.thin.internal.observer.ObservableEventType
 import io.split.client.thin.internal.observer.Observer
 
 internal class EventManagerObserver(
@@ -16,10 +17,10 @@ internal class EventManagerObserver(
 
     companion object {
         private val EVENT_TYPE_MAP = mapOf(
-            "eval_storage_updated" to SdkInternalEvent.EVALUATIONS_SYNC_COMPLETE,
-            "eval_loaded_from_storage" to SdkInternalEvent.EVALUATIONS_LOADED_FROM_STORAGE,
-            "sdk_ready_timeout_reached" to SdkInternalEvent.SDK_READY_TIMEOUT_REACHED,
-            "evaluations_updated" to SdkInternalEvent.EVALUATIONS_UPDATED,
+            ObservableEventType.EVAL_STORAGE_UPDATED to SdkInternalEvent.EVALUATIONS_SYNC_COMPLETE,
+            ObservableEventType.EVAL_LOADED_FROM_STORAGE to SdkInternalEvent.EVALUATIONS_LOADED_FROM_STORAGE,
+            ObservableEventType.SDK_READY_TIMEOUT_REACHED to SdkInternalEvent.SDK_READY_TIMEOUT_REACHED,
+            ObservableEventType.EVALUATIONS_UPDATED to SdkInternalEvent.EVALUATIONS_UPDATED,
         )
     }
 }
---
+++ observer/src/main/java/io/split/client/thin/internal/observer/ObservableEventType.kt
@@ -0,0 +1,8 @@
+package io.split.client.thin.internal.observer
+
+object ObservableEventType {
+    const val EVAL_STORAGE_UPDATED = "eval_storage_updated"
+    const val EVAL_LOADED_FROM_STORAGE = "eval_loaded_from_storage"
+    const val SDK_READY_TIMEOUT_REACHED = "sdk_ready_timeout_reached"
+    const val EVALUATIONS_UPDATED = "evaluations_updated"
+}
