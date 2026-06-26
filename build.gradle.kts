3349995615bf45ec907c7e902f8fae587b723f44
Gaston Thea <gaston.thea@harness.io>
2026-06-26 15:36:52 -0300

    Version 1.0.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,7 +23,7 @@ plugins {
 
 subprojects {
     group = "io.split.client"
-    version = "1.0.0-rc6"
+    version = "1.0.0"
 
     plugins.withId("com.android.library") {
         extensions.configure<LibraryExtension>("android") {
