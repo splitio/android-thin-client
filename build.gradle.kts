32efb2abd6b8f357e2d1d7d05ba80dd5ea8bd56f
Gaston Thea <gaston.thea@harness.io>
2026-07-20 14:19:48 +0000

    Prepare release 1.0.1 (#126)

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,7 +23,7 @@ plugins {
 
 subprojects {
     group = "io.split.client"
-    version = "1.0.1-rc1"
+    version = "1.0.1"
 
     plugins.withId("com.android.library") {
         extensions.configure<LibraryExtension>("android") {
